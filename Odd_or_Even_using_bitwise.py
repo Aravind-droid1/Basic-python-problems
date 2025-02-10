c36debad35d4c024bdd37b39b8eeaaaaa2510b5d
Aravind P <aravindshankar486@gmail.com>
2025-02-10 22:42:06 +0530

    Update Odd_or_Even_using_bitwise.py

--- Odd_or_Even_using_bitwise.py
+++ Odd_or_Even_using_bitwise.py
@@ -1,10 +1,12 @@
+#finding odd or even using bit operator
 class number(object):
     def odd_or_even(self,n):
+        #checks last bit of the number n
         if (n&1):
             print("odd number")
         else:
             print("even number")
-            
+
 obj=number()
 obj.odd_or_even(3)
 obj.odd_or_even(4)
