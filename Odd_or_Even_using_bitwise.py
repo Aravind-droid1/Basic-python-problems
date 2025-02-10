8192ecf0f536ebd6a612b1a13bf2c2012246d5f1
Aravind P <aravindshankar486@gmail.com>
2025-02-10 22:19:30 +0530

    Add files via upload

---
+++ Odd_or_Even_using_bitwise.py
@@ -0,0 +1,13 @@
+class number(object):
+    def odd_or_even(self,n):
+        if (n&1):
+            print("odd number")
+        else:
+            print("even number")
+            
+obj=number()
+obj.odd_or_even(3)
+obj.odd_or_even(4)
+num1=number()
+n=int(input("enter a number:"))
+num1.odd_or_even(n)
