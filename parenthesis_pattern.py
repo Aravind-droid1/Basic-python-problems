518fb784da1a7e4beff47ca8faae61d0754900fe
Aravind P <aravindshankar486@gmail.com>
2025-02-15 23:27:20 +0530

    Create parenthesis_pattern.py

---
+++ parenthesis_pattern.py
@@ -0,0 +1,9 @@
+class para(object):
+    def parenthesis(self,n):
+        for i in range(n):
+            for j in range(i+1):
+                print('{'*j,'}'*j,end=" ")
+            print()
+
+obj=para()
+obj.parenthesis(5)
